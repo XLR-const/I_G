851ecbfb9e378e19bb6166ff17cb27a4f19543fc
XLR-const <ilausakov36@gmail.com>
2026-07-19 13:11:37 +0400

    small fix to name config

--- config/game_data.py
+++ config/game_data.py
@@ -23,14 +23,14 @@ SYMBOLS_CONFIG = {
     'N': {'type': 'wall', 'texture': 'resources/textures/N.png'},
     
     # Предметы
-    'h': {
+    'health': {
         'type': 'item',
         'item_type': 'health',
         'amount': 25,
         'sprite': 'resources/items/health.png',
         'description': 'Аптечка (+25 HP)'
     },
-    'a': {
+    'armor': {
         'type': 'item',
         'item_type': 'armor',
         'amount': 25,
@@ -38,7 +38,7 @@ SYMBOLS_CONFIG = {
         'description': 'Броня (+25 Armor)'
     },
     
-    'k': {
+    'ak47': {
         'type': 'item',
         'item_type': 'weapon',
         'weapon_name': 'AK-47',
@@ -46,7 +46,7 @@ SYMBOLS_CONFIG = {
         'sprite': 'resources/weapons/AK47/icon.png',  # Используем готовую иконку из папки пушки!
         'description': 'Автомат АК-47 (+30 патронов)'
     },
-    'c': {
+    'colt': {
         'type': 'item',
         'item_type': 'weapon',
         'weapon_name': 'COLT',
@@ -54,7 +54,7 @@ SYMBOLS_CONFIG = {
         'sprite': 'resources/weapons/Colt/icon.png',  # Иконка Кольта
         'description': 'Пистолет Colt 1911 (+7 патронов)'
     },
-    'g': {
+    'shotgun': {
         'type': 'item',
         'item_type': 'weapon',
         'weapon_name': 'COCH',
@@ -62,7 +62,7 @@ SYMBOLS_CONFIG = {
         'sprite': 'resources/weapons/COCH/icon.png',  # Иконка двустволки
         'description': 'Двустволка Super Shotgun (+4 патрона)'
     },
-    'p': {
+    'plasmagun': {
         'type': 'item',
         'item_type': 'weapon',
         'weapon_name': 'PLASMA',
@@ -73,20 +73,20 @@ SYMBOLS_CONFIG = {
 
     
     # Дверь
-    'D': {'type': 'door', 'texture': 'resources/textures/D.png'},
+    'Door': {'type': 'door', 'texture': 'resources/textures/D.png'},
     
     # Выход
-    'E': {'type': 'exit'},
+    'Exit': {'type': 'exit'},
     
     # Спавн игрока
-    'S': {'type': 'player_spawn'},
+    'Spawn': {'type': 'player_spawn'},
 }
 
 # ============================================================
 # КОНФИГУРАЦИЯ NPC
 # ============================================================
 NPC_CONFIG = {
-        '2': {
+        'AGG': {
         'name': 'AutoGunGuy',
         'speed': 0.28,
         'hp': 100,
@@ -98,7 +98,7 @@ NPC_CONFIG = {
         'shoot_delay': 1400,
         'sound_volume': 0.25,
     },
-        '3': {
+        'BS': {
         'name': 'BeamSolder',
         'speed': 0.28,
         'hp': 100,
@@ -110,7 +110,7 @@ NPC_CONFIG = {
         'shoot_delay': 2000,
         'sound_volume': 0.25,
     },
-        '4': {
+        'CM': {
         'name': 'ChaingunMajor',        # Должно строго совпадать с именем папки!
         'speed': 0.18,                 # Медлительный из-за тяжелого пулемета
         'hp': 150,                     # Живучий мини-босс
@@ -122,7 +122,7 @@ NPC_CONFIG = {
         'shoot_delay': 2200,           # Задержка МЕЖДУ длинными очередями
         'sound_volume': 0.3,
     },
-    'B': {
+    'HS': {
         'name': 'HellSmith',            # Имя папки со спрайтами и logic.py
         'speed': 0.15,                  # Идет медленно, но неумолимо, сотрясая пол
         'hp': 2000,                     # Огромный запас здоровья для долгого боя
@@ -134,7 +134,7 @@ NPC_CONFIG = {
         'shoot_delay': 1800,            # Кулдаун в миллисекундах МЕЖДУ его супер-атаками
         'sound_volume': 0.45,           # Слышно на весь уровень!
     },
-        '5': {
+        'SB': {
         'name': 'SuicideBomber',
         'speed': 0.48,        # Быстрый как пуля!
         'hp': 80,             # Мало здоровья, чтобы игрок успевал сбрить его на подлете
